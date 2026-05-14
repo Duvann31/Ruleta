48668949937a1d97524cf6f5d9f651215e978c06
Duvan <sanchezduvan421@gmail.com>
2026-05-14 13:10:13 -0500

    guardar

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int dinero;
+        int dinero=0;
+        Scanner teclado = new Scanner(System.in);
+
     }
 }
